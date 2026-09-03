500cd0dc3be1def41df30fb9d984d4059ffa02c1
Henrique Segundo <henriquesegundo2000@gmail.com>
2026-09-03 09:00:59 -0300

    comentario close

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public static void q02(){
     Scanner in = new Scanner(System.in);
     System.out.print("Digite um número: ");
     int numero = in.nextInt();
-    in.close();
+    //in.close();
 
     System.out.println("O número informado foi: " + numero);
 }
@@ -24,7 +24,7 @@ public static void q03(){
     float n2 = in.nextFloat();
     System.out.print("Digite a nota 3: ");
     float n3 = in.nextFloat();
-    in.close();
+    //in.close();
 
     float media = (n1+n2+n3)/3;
     System.out.println("Média das notas: " + media);
@@ -34,7 +34,7 @@ public static void q04(){
     Scanner in = new Scanner(System.in);
     System.out.print("Digite um valor em metros: ");
     float metros = in.nextFloat();
-    in.close();
+    //in.close();
     float centimetros = metros * 100;
     System.out.println("Valor convertido: "+ centimetros +" cm");
 }
@@ -44,7 +44,7 @@ public static void q05(){
     Scanner in = new Scanner(System.in);
     System.out.print("Digite o raio do circulo: ");
     float raio = in.nextFloat();
-    in.close();
+    //in.close();
     
     System.out.print("Área do circulo: " + Math.PI * raio * raio);
 }
@@ -57,7 +57,7 @@ public static void q06(){
     float valor2 = in.nextFloat();
     System.out.print("Digite o valor do 3° produto: ");
     float valor3 = in.nextFloat();
-    in.close();
+    //in.close();
     
     if (valor1 < valor2) {
         if(valor1 < valor3) {
